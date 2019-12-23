5e755be789b1b009c73b5b6e87840813b83111b2
Muhammad Khan <MuhammadKhan1607@gmail.com>
2019-12-23 18:10:16 +0500

    Fix the microsecond parsing

--- qualifier.py
+++ qualifier.py
@@ -32,25 +32,33 @@ def parse_iso8601_date(date: str) -> datetime.date:
 def parse_iso8601_time(time: str) -> datetime.time:
     """Parse ISO-8601 formatted time"""
     #Get the microsecond
-    if "." in time and len(time) in (10, 12):
-        time, microsecond = time.split(".")
+    if len(time) in (10, 12, 15):
+        if "." in time:
+            time, second_factor = time.split(".")
+        elif "," in time:
+            time, second_factor = time.split(",")
+        if len(second_factor) == 3:
+            microsecond = int(second_factor) * 1000
+        else:
+            microsecond = int(second_factor)
     else:
         microsecond = 0
+
     len_time = len(time)
 
     if ":" in time:
         #hh:mm:ss
         if len_time == 8:
-            datetime.time(*map(int, time.split(":")), microsecond)
+            return datetime.time(*map(int, time.split(":")), microsecond)
         #hh:mm
         elif len_time == 5:
-            datetime.time(*map(int, time.split(":")))
+            return datetime.time(*map(int, time.split(":")))
     #hhmmss
     elif len_time == 6:
-        datetime.time(*map(int, (time[:2], time[2:4], time[4:])), microsecond)
+        return datetime.time(*map(int, (time[:2], time[2:4], time[4:])), microsecond)
     #hhmm
     elif len_time == 4:
-        datetime.time(*map(int, *(time[:2], time[2:])))
+        return datetime.time(*map(int, *(time[:2], time[2:])))
     #hh
     elif len_time == 2:
         return datetime.time(int(time))
@@ -79,6 +87,7 @@ if __name__ == "__main__":
     assert parse_iso8601("2019-12-16") == datetime.datetime(2019, 12, 16, 0, 0), "YYYY-MM-DD"
     assert parse_iso8601("2017-01-08T12") == datetime.datetime(2017, 1, 8, 12, 0), "YYYY-MM-DDThh"
     assert parse_iso8601("2017-01-08T12") == datetime.datetime(2017, 1, 8, 12, 0), "YYYY-MM-DDThh"
+    assert parse_iso8601("2010-02-18T16:23:48,444") == datetime.datetime(2010, 2, 18, 16, 23, 48, 444000)
     try:
         parse_iso8601("2000-10-16T09:23:61")
     except ValueError as e:
