8bc4623bfe4589ea43f5c1af1773a4f0ebbc7424
Muhammad Khan <MuhammadKhan1607@gmail.com>
2020-01-12 11:19:30 +0500

    Add all the new test and customize the setup
    - Add tests for fractional seconds
    - Add tests for fractional minutes
    - Add tests for fractional hours
    - Add tests for more datetimes
    - Change spaces to tabs
    - Customize the styling

--- qualifier.py
+++ qualifier.py
@@ -2,163 +2,162 @@ import datetime as dt
 from functools import lru_cache
 
 
-ORDINAL_TABLE = [0, 31, 59, 90, 120, 151, 181,
-                 212, 243, 273, 304, 334]
+ORDINAL_TABLE = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
 
 
 @lru_cache(maxsize=8)
 def _is_leap_year(year: int) -> bool:
-    """Determine whether a year is a leap year"""
-    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+	"""Determine whether a year is a leap year"""
+	return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 
 
 @lru_cache(maxsize=8)
 def _days_in_year(year: int) -> int:
-    """Get the number of days in a year"""
-    return 365 + _is_leap_year(year)
+	"""Get the number of days in a year"""
+	return 365 + _is_leap_year(year)
 
 
 def _validate_format(date: str, time: str) -> None:
-    """Raise a value error if date and time are not both in the same format"""
-    if "-" in date and ":" not in time and len(time) != 2 or "-" not in date and ":" in time:
-        raise ValueError("Cannot mix extended and basic format")
+	"""Raise a value error if date and time are not both in the same format"""
+	if "-" in date and ":" not in time and len(time) != 2 or "-" not in date and ":" in time:
+		raise ValueError("Cannot mix extended and basic format")
 
 
 def _get_date_from_ordinal(year: int, ordinal: int) -> dt.date:
-    """Get date from the ordinal day and year"""
-    if ordinal < 1 or ordinal > _days_in_year(year):
-        raise ValueError("Invalid ordinal day")
+	"""Get date from the ordinal day and year"""
+	if ordinal < 1 or ordinal > _days_in_year(year):
+		raise ValueError("Invalid ordinal day")
 
-    day = ordinal
-    for i, x in enumerate(ORDINAL_TABLE):
-        if ordinal <= x:
-            break
-        month = i + 1
-        day = ordinal - x
-    if _is_leap_year(year) and month > 2:
-        day -= 1
-    return dt.date(year, month, day)
+	day = ordinal
+	for i, x in enumerate(ORDINAL_TABLE):
+		if ordinal <= x:
+			break
+		month = i + 1
+		day = ordinal - x
+	if _is_leap_year(year) and month > 2:
+		day -= 1
+	return dt.date(year, month, day)
 
 
 def _get_date_from_week_date(year: int, week: int, weekday: int) -> dt.date:
-    """Get date from week number and weekday"""
-    if weekday < 1 or weekday > 7:
-        raise ValueError("Invalid weekday")
-    elif week < 1 or week > 53:
-        raise ValueError("Invalid week")
+	"""Get date from week number and weekday"""
+	if weekday < 1 or weekday > 7:
+		raise ValueError("Invalid weekday")
+	elif week < 1 or week > 53:
+		raise ValueError("Invalid week")
 
-    ordinal = week * 7 + weekday - (dt.date(year, 1, 4).weekday() + 4)
-    if ordinal < 1:
-        year -= 1
-        ordinal += _days_in_year(year)
-    elif ordinal > _days_in_year(year):
-        ordinal -= _days_in_year(year)
-        year += 1
-    return _get_date_from_ordinal(year, ordinal)
+	ordinal = week * 7 + weekday - (dt.date(year, 1, 4).weekday() + 4)
+	if ordinal < 1:
+		year -= 1
+		ordinal += _days_in_year(year)
+	elif ordinal > _days_in_year(year):
+		ordinal -= _days_in_year(year)
+		year += 1
+	return _get_date_from_ordinal(year, ordinal)
 
 
 def _parse_date(date: str) -> dt.date:
-    """Parse ISO-8601 formatted date"""
-    len_date = len(date)
+	"""Parse ISO-8601 formatted date"""
+	len_date = len(date)
 
-    if len_date == 10:
-        # YYYY-Www-D
-        if "W" in date:
-            return _get_date_from_week_date(int(date[:4]), int(date[6:8]), int(date[9]))
-        else:
-            return dt.date(*map(int, date.split("-")))
-    elif len_date == 8:
-        # YYYY-DDD
-        if "-" in date:
-            return _get_date_from_ordinal(*map(int, date.split("-")))
-        # YYYYWwwD
-        elif "W" in date:
-            return _get_date_from_week_date(int(date[:4]), int(date[5:7]), int(date[7]))
-        # YYYYMMDD
-        else:
-            return dt.date(*map(int, (date[:4], date[4:6], date[6:])))
-    # YYYYDDD
-    elif len_date == 7:
-        return _get_date_from_ordinal(int(date[:4]), int(date[4:]))
-    else:
-        raise ValueError("Invalid ISO-8601 format for date")
+	if len_date == 10:
+		# YYYY-Www-D
+		if "W" in date:
+			return _get_date_from_week_date(int(date[:4]), int(date[6:8]), int(date[9]))
+		else:
+			return dt.date(*map(int, date.split("-")))
+	elif len_date == 8:
+		# YYYY-DDD
+		if "-" in date:
+			return _get_date_from_ordinal(*map(int, date.split("-")))
+		# YYYYWwwD
+		elif "W" in date:
+			return _get_date_from_week_date(int(date[:4]), int(date[5:7]), int(date[7]))
+		# YYYYMMDD
+		else:
+			return dt.date(*map(int, (date[:4], date[4:6], date[6:])))
+	# YYYYDDD
+	elif len_date == 7:
+		return _get_date_from_ordinal(int(date[:4]), int(date[4:]))
+	else:
+		raise ValueError("Invalid ISO-8601 format for date")
 
 
 def _parse_time(time: str) -> dt.time:
-    """Parse ISO-8601 formatted time"""
+	"""Parse ISO-8601 formatted time"""
 
-    if "Z" in time:
-        time = time.strip("Z")
-        tzinfo = dt.timezone.utc
-    elif "+" in time or "-" in time:
-        for sign in ("+", "-"):
-            if sign in time:
-                time, utc = time.split(sign)
-                if ":" in utc:
-                    hours = int(sign + utc[:2])
-                    minutes = int(sign + utc[3:])
-                elif len(utc) == 4:
-                    hours = int(sign + utc[:2])
-                    minutes = int(sign + utc[2:])
-                else:
-                    hours = int(sign + utc)
-                    minutes = 0
-                # As far as I can tell current min max timezones are -12 and +14 but some historical
-                # timezones were larger so the limit is -16 to 16
-                if hours < -16 or hours > 16:
-                    raise ValueError("Invalid hour value for utcoffset")
-                elif minutes < -59 or minutes > 59:
-                    raise ValueError("Invalid minutes value for utcoffset")
-                tzinfo = dt.timezone(dt.timedelta(hours=hours, minutes=minutes))
-    else:
-        tzinfo = None
+	if "Z" in time:
+		time = time.strip("Z")
+		tzinfo = dt.timezone.utc
+	elif "+" in time or "-" in time:
+		for sign in ("+", "-"):
+			if sign in time:
+				time, utc = time.split(sign)
+				if ":" in utc:
+					hours = int(sign + utc[:2])
+					minutes = int(sign + utc[3:])
+				elif len(utc) == 4:
+					hours = int(sign + utc[:2])
+					minutes = int(sign + utc[2:])
+				else:
+					hours = int(sign + utc)
+					minutes = 0
+				# As far as I can tell current min max timezones are -12 and +14 but some historical
+				# timezones were larger so the limit is -16 to 16
+				if hours < -16 or hours > 16:
+					raise ValueError("Invalid hour value for utcoffset")
+				elif minutes < -59 or minutes > 59:
+					raise ValueError("Invalid minutes value for utcoffset")
+				tzinfo = dt.timezone(dt.timedelta(hours=hours, minutes=minutes))
+	else:
+		tzinfo = None
 
-    # Get the microsecond
-    if len(time) in (10, 12, 15) and ("." in time or "," in time):
-        if "." in time:
-            time, second_factor = time.split(".")
-        elif "," in time:
-            time, second_factor = time.split(",")
-        if len(second_factor) == 3:
-            microsecond = int(second_factor) * 1000
-        else:
-            microsecond = int(second_factor)
-    else:
-        microsecond = 0
+	# Get the microsecond
+	if len(time) in (10, 12, 15) and ("." in time or "," in time):
+		if "." in time:
+			time, second_factor = time.split(".")
+		elif "," in time:
+			time, second_factor = time.split(",")
+		if len(second_factor) == 3:
+			microsecond = int(second_factor) * 1000
+		else:
+			microsecond = int(second_factor)
+	else:
+		microsecond = 0
 
-    len_time = len(time)
+	len_time = len(time)
 
-    if ":" in time:
-        # hh:mm:ss
-        if len_time == 8:
-            return dt.time(*map(int, time.split(":")), microsecond, tzinfo)
-        # hh:mm
-        elif len_time == 5:
-            return dt.time(*map(int, time.split(":")), tzinfo=tzinfo)
-        else:
-            raise ValueError("Invalid ISO-8601 format for time")
-    # hhmmss
-    elif len_time == 6:
-        return dt.time(*map(int, (time[:2], time[2:4], time[4:])), microsecond, tzinfo)
-    # hhmm
-    elif len_time == 4:
-        return dt.time(*map(int, *(time[:2], time[2:])), tzinfo=tzinfo)
-    # hh
-    elif len_time == 2:
-        return dt.time(int(time), tzinfo=tzinfo)
-    else:
-        raise ValueError("Invalid ISO-8601 format for time")
+	if ":" in time:
+		# hh:mm:ss
+		if len_time == 8:
+			return dt.time(*map(int, time.split(":")), microsecond, tzinfo)
+		# hh:mm
+		elif len_time == 5:
+			return dt.time(*map(int, time.split(":")), tzinfo=tzinfo)
+		else:
+			raise ValueError("Invalid ISO-8601 format for time")
+	# hhmmss
+	elif len_time == 6:
+		return dt.time(*map(int, (time[:2], time[2:4], time[4:])), microsecond, tzinfo)
+	# hhmm
+	elif len_time == 4:
+		return dt.time(*map(int, *(time[:2], time[2:])), tzinfo=tzinfo)
+	# hh
+	elif len_time == 2:
+		return dt.time(int(time), tzinfo=tzinfo)
+	else:
+		raise ValueError("Invalid ISO-8601 format for time")
 
 
 def parse_iso8601(timestamp: str) -> dt.datetime:
-    """Parse an ISO-8601 formatted time stamp."""
-    if "T" in timestamp:
-        date, time = timestamp.split("T")
-        _validate_format(date, time)
-        time = _parse_time(time)
-    else:
-        date = timestamp
-        time = dt.time.min
-    date = _parse_date(date)
+	"""Parse an ISO-8601 formatted time stamp."""
+	if "T" in timestamp:
+		date, time = timestamp.split("T")
+		_validate_format(date, time)
+		time = _parse_time(time)
+	else:
+		date = timestamp
+		time = dt.time.min
+	date = _parse_date(date)
 
-    return dt.datetime.combine(date, time)
+	return dt.datetime.combine(date, time)
--- qualifier_test.py
+++ qualifier_test.py
@@ -6,64 +6,88 @@ from qualifier import parse_iso8601
 
 
 def test_dates():
-    assert parse_iso8601("20170505") == dt.datetime(2017, 5, 5, 0, 0), "YYYYMMDD"
-    assert parse_iso8601("2019-12-16") == dt.datetime(2019, 12, 16, 0, 0), "YYYY-MM-DD"
-    assert parse_iso8601("0001-01-01") == dt.datetime(1, 1, 1, 0, 0), "First day of the Common Era"
-    assert parse_iso8601("9999-12-31") == dt.datetime(9999, 12, 31, 0, 0), "Last day"
+	assert parse_iso8601("20170505") == dt.datetime(2017, 5, 5, 0, 0), "YYYYMMDD"
+	assert parse_iso8601("2019-12-16") == dt.datetime(2019, 12, 16, 0, 0), "YYYY-MM-DD"
+	assert parse_iso8601("0001-01-01") == dt.datetime(1, 1, 1, 0, 0), "First day of the Common Era"
+	assert parse_iso8601("9999-12-31") == dt.datetime(9999, 12, 31, 0, 0), "Last day"
 
 
 def test_datetimes():
-    assert parse_iso8601("2017-01-08T12") == dt.datetime(2017, 1, 8, 12, 0), "YYYY-MM-DDThh"
-    assert parse_iso8601("2017-01-08T12") == dt.datetime(2017, 1, 8, 12, 0), "YYYY-MM-DDThh"
-    assert parse_iso8601("2010-02-18T16:23:48,444") == dt.datetime(2010, 2, 18, 16, 23, 48, 444000), "YYYY-MM-DDThh:mm:ss,sss"
-    assert parse_iso8601("2010-02-18T16:23:48.444") == dt.datetime(2010, 2, 18, 16, 23, 48, 444000), "YYYY-MM-DDThh:mm:ss.sss"
-    assert parse_iso8601("1890-06-06T05:09:45.123456") == dt.datetime(1890, 6, 6, 5, 9, 45, 123456), "YYYY-MM-DDThh:mm:ss.ssssss"
-    assert parse_iso8601("1890-06-06T05:09:45,123456") == dt.datetime(1890, 6, 6, 5, 9, 45, 123456), "YYYY-MM-DDThh:mm:ss,ssssss"
+	assert parse_iso8601("2017-01-08T12") == dt.datetime(2017, 1, 8, 12, 0), "YYYY-MM-DDThh"
+	assert parse_iso8601("2017-01-08T12") == dt.datetime(2017, 1, 8, 12, 0), "YYYY-MM-DDThh"
+	assert parse_iso8601('19741219T1313') == dt.datetime(1974, 12, 19, 13, 13), "YYYYMMDDThhmm"
+	assert parse_iso8601('19711103T2022') == dt.datetime(1971, 11, 3, 20, 22), "YYYYMMDDThhmm"
+
+
+def test_fractional_seconds():
+	assert parse_iso8601('18620123T111233.9') == dt.datetime(1862, 1, 23, 11, 12, 33, 900000), "YYYYMMDDThhmmss.s"
+	assert parse_iso8601('18151102T091021.98') == dt.datetime(1815, 11, 2, 9, 10, 21, 980000), "YYYYMMDDThhmmss.ss"
+	assert parse_iso8601("2010-02-18T16:23:48,444") == dt.datetime(2010, 2, 18, 16, 23, 48, 444000), "YYYY-MM-DDThh:mm:ss,sss"
+	assert parse_iso8601("2010-02-18T16:23:48.444") == dt.datetime(2010, 2, 18, 16, 23, 48, 444000), "YYYY-MM-DDThh:mm:ss.sss"
+	assert parse_iso8601('19300511T192349.9876') == dt.datetime(1930, 5, 11, 19, 23, 49, 987600), "YYYYMMDDhhmmss.ssss"
+	assert parse_iso8601('19420101T235959.98765') == dt.datetime(1942, 1, 1, 23, 59, 59, 987650), "YYYYMMDDThhmmss.sssss"
+	assert parse_iso8601("1890-06-06T05:09:45.123456") == dt.datetime(1890, 6, 6, 5, 9, 45, 123456), "YYYY-MM-DDThh:mm:ss.ssssss"
+	assert parse_iso8601("1890-06-06T05:09:45,123456") == dt.datetime(1890, 6, 6, 5, 9, 45, 123456), "YYYY-MM-DDThh:mm:ss,ssssss"
+	assert parse_iso8601('19410909T111111.987654') == dt.datetime(1941, 9, 9, 11, 11, 11, 987654), "YYYYMMDDThhmmss.ssssss"
+
+
+def test_fractional_minutes():
+	assert parse_iso8601('1905-08-16T19:15.000001') == dt.datetime(1905, 8, 16, 19, 15, 0, 60), "YYYY-MM-DDThh:mm.mmmmmm"
+	assert parse_iso8601('1715-10-08T00:02.125') == dt.datetime(1715, 10, 8, 0, 2, 7, 500000), "YYYY-MM-DD-Thh:mm.mmm"
+	assert parse_iso8601('1905-08-16T19:15.078125') == dt.datetime(1905, 8, 6, 19, 15, 4, 687500), "YYYY-MM-DDTThh:mm.mmmmmm"
+
+
+def test_fractional_hours():
+	assert parse_iso8601('1999-09-09T12.5') == dt.datetime(1999, 9, 9, 12, 30), "YYYY-MM-DDThh.h"
+	assert parse_iso8601('1715-10-08T12.125') == dt.datetime(1715, 10, 8, 12, 7, 30), "YYYY-MM-DDThh.hhh"
+	assert parse_iso8601('1905-08-16T16.03125') == dt.datetime(1905, 8, 16, 16, 1, 52, 500000), "YYYY-MM-DDThh.hhhhh"
+	assert parse_iso8601('1905-08-16T16.015625') == dt.datetime(1905, 8, 16, 16, 0, 56, 250000), "YYYY-MM-DDThh.hhhhhh"
+	assert parse_iso8601('1905-08-16T16.000001') == dt.datetime(1905, 8, 16, 16, 0, 0, 3600), "YYYY-MM-DDThh.hhhhhhh"
 
 
 def test_timezones():
-    assert parse_iso8601("2017-05-05T12:00:00Z") == dt.datetime(2017, 5, 5, 12, 0, 0, 0, dt.timezone.utc), "YYYY-MM-DDThh:mm:ssZ"
-    assert parse_iso8601("1992-10-27T12+05:30") == dt.datetime(1992, 10, 27, 12, 0, 0, 0, dt.timezone(dt.timedelta(hours=5, minutes=30))), "YYYY-MM-DDThh+hh:mm"
-    assert parse_iso8601("1994-04-22T12:30-11") == dt.datetime(1994, 4, 22, 12, 30, 0, 0, dt.timezone(dt.timedelta(hours=-11))), "YYYY-MM-DDThh:mm-hh"
+	assert parse_iso8601("2017-05-05T12:00:00Z") == dt.datetime(2017, 5, 5, 12, 0, 0, 0, dt.timezone.utc), "YYYY-MM-DDThh:mm:ssZ"
+	assert parse_iso8601("1992-10-27T12+05:30") == dt.datetime(1992, 10, 27, 12, 0, 0, 0, dt.timezone(dt.timedelta(hours=5, minutes=30))), "YYYY-MM-DDThh+hh:mm"
+	assert parse_iso8601("1994-04-22T12:30-11") == dt.datetime(1994, 4, 22, 12, 30, 0, 0, dt.timezone(dt.timedelta(hours=-11))), "YYYY-MM-DDThh:mm-hh"
 
 
 def test_ordinal_dates():
-    assert parse_iso8601("2019-253") == dt.datetime(2019, 9, 10), "YYYY-DDD"
-    assert parse_iso8601("2019031") == dt.datetime(2019, 1, 31), "YYYYDDD"
-    assert parse_iso8601("2019001") == dt.datetime(2019, 1, 1), "First ordinal day"
-    assert parse_iso8601("2019-365") == dt.datetime(2019, 12, 31), "Last ordinal day"
-    assert parse_iso8601("1956-366") == dt.datetime(1956, 12, 31), "Leap Year last ordinal day"
+	assert parse_iso8601("2019-253") == dt.datetime(2019, 9, 10), "YYYY-DDD"
+	assert parse_iso8601("2019031") == dt.datetime(2019, 1, 31), "YYYYDDD"
+	assert parse_iso8601("2019001") == dt.datetime(2019, 1, 1), "First ordinal day"
+	assert parse_iso8601("2019-365") == dt.datetime(2019, 12, 31), "Last ordinal day"
+	assert parse_iso8601("1956-366") == dt.datetime(1956, 12, 31), "Leap Year last ordinal day"
 
 
 def test_week_dates():
-    assert parse_iso8601("2019-W52-2") == dt.datetime(2019, 12, 24), "YYYY-Www-D"
-    assert parse_iso8601("2019W521") == dt.datetime(2019, 12, 23), "YYYYWwwD"
-    assert parse_iso8601("2009-W01-1") == dt.datetime(2008, 12, 29), "YYYY-Www-D Week 1 edge case"
-    assert parse_iso8601("2009W537") == dt.datetime(2010, 1, 3), "YYYYWwwD Week 53 Sun"
-    assert parse_iso8601("2004-W53-6") == dt.datetime(2005, 1, 1), "YYYY-Www-D Week 53 Sat"
+	assert parse_iso8601("2019-W52-2") == dt.datetime(2019, 12, 24), "YYYY-Www-D"
+	assert parse_iso8601("2019W521") == dt.datetime(2019, 12, 23), "YYYYWwwD"
+	assert parse_iso8601("2009-W01-1") == dt.datetime(2008, 12, 29), "YYYY-Www-D Week 1 edge case"
+	assert parse_iso8601("2009W537") == dt.datetime(2010, 1, 3), "YYYYWwwD Week 53 Sun"
+	assert parse_iso8601("2004-W53-6") == dt.datetime(2005, 1, 1), "YYYY-Www-D Week 53 Sat"
 
 
 def test_invalids():
-    tst_value_errors("2000-10-16T09:23:61", "second must be in 0..59")
-    tst_value_errors("2000-10-16Z", "Invalid ISO-8601 format for date")
-    tst_value_errors("2000-10-16T09:23:59+25", "Invalid hour value for utcoffset")
-    tst_value_errors("2000-10-16T09:23:06.666-00:60", "Invalid minutes value for utcoffset")
-    tst_value_errors("2001-13-16T09:23:06.666-00:30", "month must be in 1..12")
-    tst_value_errors("2001-12-32T09:23:06.666-00:30", "day is out of range for month")
-    tst_value_errors("2019-W52-8", "Invalid weekday")
-    tst_value_errors("1909-W01-0", "Invalid weekday")
-    tst_value_errors("2019-W54-7", "Invalid week")
-    tst_value_errors("2019-W00-7", "Invalid week")
-    tst_value_errors("2001-366", "Invalid ordinal day")
-    tst_value_errors("2004-000", "Invalid ordinal day")
-    tst_value_errors("2001-12-31T09:23:06.63-00:30", "Invalid ISO-8601 format for time")
-    tst_value_errors("2001-07-16T09.12:23:06-00:30", "Invalid ISO-8601 format for time")
-    tst_value_errors("2001-07-16T09:23.23:06-00:30", "Invalid ISO-8601 format for time")
-    tst_value_errors("20010716T09:24:25Z", "Cannot mix extended and basic format")
-    tst_value_errors("1999-04-05T092459", "Cannot mix extended and basic format")
+	tst_value_errors("2000-10-16T09:23:61", "second must be in 0..59")
+	tst_value_errors("2000-10-16Z", "Invalid ISO-8601 format for date")
+	tst_value_errors("2000-10-16T09:23:59+25", "Invalid hour value for utcoffset")
+	tst_value_errors("2000-10-16T09:23:06.666-00:60", "Invalid minutes value for utcoffset")
+	tst_value_errors("2001-13-16T09:23:06.666-00:30", "month must be in 1..12")
+	tst_value_errors("2001-12-32T09:23:06.666-00:30", "day is out of range for month")
+	tst_value_errors("2019-W52-8", "Invalid weekday")
+	tst_value_errors("1909-W01-0", "Invalid weekday")
+	tst_value_errors("2019-W54-7", "Invalid week")
+	tst_value_errors("2019-W00-7", "Invalid week")
+	tst_value_errors("2001-366", "Invalid ordinal day")
+	tst_value_errors("2004-000", "Invalid ordinal day")
+	tst_value_errors("2001-12-31T09:23:06.63-00:30", "Invalid ISO-8601 format for time")
+	tst_value_errors("2001-07-16T09.12:23:06-00:30", "Invalid ISO-8601 format for time")
+	tst_value_errors("2001-07-16T09:23.23:06-00:30", "Invalid ISO-8601 format for time")
+	tst_value_errors("20010716T09:24:25Z", "Cannot mix extended and basic format")
+	tst_value_errors("1999-04-05T092459", "Cannot mix extended and basic format")
 
 
 def tst_value_errors(value, error):
-    with pytest.raises(ValueError) as excinfo:
-        parse_iso8601(value)
-    assert str(excinfo.value) == error
+	with pytest.raises(ValueError) as excinfo:
+		parse_iso8601(value)
+	assert str(excinfo.value) == error
